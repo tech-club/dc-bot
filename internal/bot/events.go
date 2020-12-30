4316c543c19fb53e8272290f54b86972e5294c05
bitcubix <bitcubix@protonmail.ch>
2020-12-30 14:21:57 +0100

    move event registration to bot package

---
+++ internal/bot/events.go
@@ -0,0 +1,11 @@
+package bot
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/tech-club/dc-bot/internal/events"
+	"github.com/tech-club/dc-bot/pkg/log"
+)
+
+func registerEvents(s *discordgo.Session, log log.Logger) {
+	s.AddHandler(events.NewReadyHandler(log))
+}
