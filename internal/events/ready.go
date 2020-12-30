b54b31081534d028598c80af844753cf5906925f
bitcubix <bitcubix@protonmail.ch>
2020-12-30 10:37:58 +0100

    add ReadyEventHandler

---
+++ internal/events/ready.go
@@ -0,0 +1,17 @@
+package events
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+type ReadyHandler struct{}
+
+func NewReadyHandler() *ReadyHandler {
+	return &ReadyHandler{}
+}
+
+func (h *ReadyHandler) Handler(_ *discordgo.Session, e *discordgo.Ready) {
+	fmt.Println("Bot session is ready")
+	fmt.Printf("Logged in as %s\n", e.User.String())
+}
